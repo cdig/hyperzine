5700538635049a477b013c43e11da6434d825698
Ivan Reese <ivanreese@gmail.com>
2021-09-03 12:40:29 -0600

    Let's test some releases

--- forge.config.js
+++ forge.config.js
@@ -28,12 +28,10 @@ module.exports = {
   },
   makers: [
     {
-      name: "@electron-forge/maker-squirrel",
-      config: {}
+      name: "@electron-forge/maker-squirrel"
     },
     {
-      name: "@electron-forge/maker-zip",
-      platforms: ["darwin"]
+      name: "@electron-forge/maker-zip"
     }
   ],
   publishers: [
